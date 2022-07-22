9c8af17a84f7f0b4fe5ca012e2a69abd4007f85b
renardelyon <renard.elyon.r@gmail.com>
2022-07-22 09:37:08 +0700

    adding authorization header to swagger

--- domain/auth/handler/http_handler/get_user_by_role.go
+++ domain/auth/handler/http_handler/get_user_by_role.go
@@ -25,6 +25,10 @@ type SwaggerGetUserByRoleResponse struct {
 
 // swagger:parameters GetUserByRoleQuery
 type SwaggerGetUserByRoleQuery struct {
+	// in: header
+	// required: true
+	// example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+	Authorization string
 	// in: query
 	// example: admin
 	Role string
